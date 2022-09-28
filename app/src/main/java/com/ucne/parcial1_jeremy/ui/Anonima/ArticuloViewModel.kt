41893960a04c54555b33123265364f0b46ae3167
jeremy castillo <97201902+JeremyCastillo10@users.noreply.github.com>
2022-09-28 19:40:33 -0300

    poniendo hilt a la viewmodel

--- app/src/main/java/com/ucne/parcial1_jeremy/ui/Anonima/ArticuloViewModel.kt
+++ app/src/main/java/com/ucne/parcial1_jeremy/ui/Anonima/ArticuloViewModel.kt
@@ -7,9 +7,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ucne.parcial1_jeremy.data.entity.Articulo
 import com.ucne.parcial1_jeremy.repository.ArticuloRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+@HiltViewModel
 class ArticuloViewModel @Inject constructor(
     val repository: ArticuloRepository
 ):ViewModel() {
