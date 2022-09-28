87bd42964e6dda643ea73db1da8d7c31fc002544
jeremy castillo <97201902+JeremyCastillo10@users.noreply.github.com>
2022-09-28 19:28:08 -0300

    terminando de crear la consulta

--- app/src/main/java/com/ucne/parcial1_jeremy/ui/AnonimaList/AnonimaListViewModel.kt
+++ app/src/main/java/com/ucne/parcial1_jeremy/ui/AnonimaList/AnonimaListViewModel.kt
@@ -3,10 +3,12 @@ package com.ucne.parcial1_jeremy.ui.AnonimaList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ucne.parcial1_jeremy.data.entity.Articulo
+import com.ucne.parcial1_jeremy.repository.ArticuloRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,6 +19,7 @@ data class AnonimaListUiState(
 
 @HiltViewModel
 class AnonimaListViewModel @Inject constructor(
+    val repository: ArticuloRepository
 
 ) : ViewModel() {
 
@@ -25,6 +28,11 @@ class AnonimaListViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
+            repository.GetList().collect{list->
+                _uiState.update {
+                    it.copy()
+                }
+            }
 
         }
     }
